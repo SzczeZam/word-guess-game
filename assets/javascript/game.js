fa645df28b67b31e800ba31a331d8a65446bb030
Max Szczepaniak <wookslandline@protonmail.com>
2019-06-27 20:17:21 -0400

    code finagling. got it to replace letters at the right place. Doesn't work for duplicate letters yet

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,7 +10,9 @@ var game = {
     bank: ["word", "other", "test", "new"],
     chosenWord: "NaN",
     hangReference: [],
-    displayWord: "NaN",
+    hangAnswer: [],
+    workingWord: "NaN",
+
     attemptCount: 0,
     failedAttempts: [], 
     
@@ -22,7 +24,6 @@ var game = {
     
     convert: function () {
         this.hangReference = this.chosenWord.split("")
-        
         for (i = 0; i < this.hangReference.length; i++) {
             this.hangReference[i] = "_"
             console.log(this.hangReference)    
@@ -34,34 +35,38 @@ var game = {
     },
     
     keyCheck: function (userKey) {
-        var progressWord =  this.hangReference
+        let progressWord =  this.hangReference
+        this.hangAnswer = this.chosenWord.split(" ")
 
         for (i=0; i < this.hangReference.length; i++){
-            let charPos = this.hangReference[i].indexOf(userKey)
+            let charPos = this.hangAnswer[i].indexOf(userKey)
             // change html to display userKey press
-
+            console.log(charPos)
             if ( charPos === -1){
                 //Attempts + 1 and add userKey to array of attempts
+                console.log("that letter isn't in this word")
                 this.attemptCount++
                 this.failedAttempts.push(userKey)
             } else {
+                console.log("that letter is in this word and is at", charPos)
                 //add to progressWord
                 progressWord[charPos] = userKey
                 //convert progressWord
-                this.displayWord = progressWord.join()
-
+                this.workingWord = progressWord.join(" ")
+                this.setWord(this.workingWord)
             }
         }
     },
 
     start: function () {
-        document.onkeyup() = function(event) {
+
+        document.onkeyup = function(event) {
             var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," ",]
             var userKey = event.key
             if (alphabet.indexOf(userKey) === -1){
                 alert("I'm sorry, your input was not recognized. Please only use alphebetical keys.")
             } else {
-            this.keyCheck(userKey)
+            game.keyCheck(userKey)
             }
         }
     },
@@ -73,7 +78,8 @@ var game = {
 
 game.getWord()
 game.convert()
-game.setWord(game.displayWord)
+game.setWord(game.workingWord)
+game.start()
 
 
 
